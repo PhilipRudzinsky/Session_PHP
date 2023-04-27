32839dd26c02883fb2966355bb16d0efd134e382
Filip Rudzinski <111775935+PhilipRudzinsky@users.noreply.github.com>
2023-04-27 12:50:58 +0200

    Add files via upload

---
+++ wyloguj.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if (isset($_SESSION['log'])) {
+    unset($_SESSION['log']);
+} else {
+    header('location: loguj.php');
+    exit;
+}
+$s = session_destroy();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title> Koniec_sesji</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<p>Wylogowałeś się ze strony.</p>
+<a href="loguj.php">Logowanie</a>
+</body>
+</html>
